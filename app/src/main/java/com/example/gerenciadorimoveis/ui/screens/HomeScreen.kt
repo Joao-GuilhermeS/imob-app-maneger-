d6753e65fb5e8af11e30fc34ae898304d5222c11
João Guilherme <jguilherme.devti@gmail.com>
2026-07-02 06:54:48 -0300

    feat: cria botão e funcionalidade 'adcionar imovel'

--- app/src/main/java/com/example/gerenciadorimoveis/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gerenciadorimoveis/ui/screens/HomeScreen.kt
@@ -2,7 +2,9 @@ package com.example.gerenciadorimoveis.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -11,25 +13,40 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(onNavigateToCadastro: () -> Unit = {}) {
     val backgroundColor = Color(0xFFF8FAFC)
+    val primaryGreen = Color(0xFF22C55E)
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(backgroundColor),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "🏠 Tela Principal",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-        Text(
-            text = "A lista de imóveis será aqui testar.",
-            color = Color.Gray
-        )
+    //Scaffold p gerir o botão flutuante
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onNavigateToCadastro,
+                containerColor = primaryGreen,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Adicionar Imóvel")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(backgroundColor)
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "🏠 Tela Principal",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
+            Text(
+                text = "A lista de imóveis aparecerá aqui.",
+                color = Color.Gray
+            )
+        }
     }
 }
