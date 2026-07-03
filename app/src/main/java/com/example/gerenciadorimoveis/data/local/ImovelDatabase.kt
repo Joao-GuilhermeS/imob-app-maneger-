328214163d35b3711922d18363cbda97b97e9f24
João Guilherme <jguilherme.devti@gmail.com>
2026-07-02 23:22:41 -0300

    fix(data): adiciona padrão getDatabase no ImovelDatabase

--- app/src/main/java/com/example/gerenciadorimoveis/data/local/ImovelDatabase.kt
+++ app/src/main/java/com/example/gerenciadorimoveis/data/local/ImovelDatabase.kt
@@ -1,11 +1,31 @@
 package com.example.gerenciadorimoveis.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.gerenciadorimoveis.domain.model.Imovel
 
-@Database(entities = [Imovel :: class], version = 1, exportSchema = false)
-abstract class ImovelDatabase : RoomDatabase(){
-    // localizar as regras de acesso ao banco
+@Database(entities = [Imovel::class], version = 1, exportSchema = false)
+abstract class ImovelDatabase : RoomDatabase() {
+
     abstract fun imovelDao(): ImovelDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ImovelDatabase? = null
+
+        // funcao app navigation busca
+        fun getDatabase(context: Context): ImovelDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ImovelDatabase::class.java,
+                    "imovel_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
