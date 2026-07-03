f1ffac53ac8a6d2c7278bdfaa0912b144bbfaa2d
João Guilherme <jguilherme.devti@gmail.com>
2026-07-02 23:25:04 -0300

    feat(navigation): injeta ImovelViewModel na rota de cadastro e conecta ação de salvar

--- app/src/main/java/com/example/gerenciadorimoveis/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/gerenciadorimoveis/ui/navigation/AppNavigation.kt
@@ -1,11 +1,18 @@
 package com.example.gerenciadorimoveis.ui.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.gerenciadorimoveis.data.local.ImovelDatabase
+import com.example.gerenciadorimoveis.ui.screens.CadastroScreen
 import com.example.gerenciadorimoveis.ui.screens.HomeScreen
 import com.example.gerenciadorimoveis.ui.screens.LoginScreen
+import com.example.gerenciadorimoveis.ui.viewmodel.ImovelViewModel
 
 @Composable
 fun AppNavigation() {
@@ -31,7 +38,37 @@ fun AppNavigation() {
 
         // 2a rota -> Tela Principal
         composable("home") {
-            HomeScreen()
+            HomeScreen(
+                // Ligação simples: clica no "+" e abre a tela de cadastro
+                onNavigateToCadastro = {
+                    navController.navigate("cadastro")
+                }
+            )
+        }
+
+        // 3a rota -> Tela de Cadastro
+        composable("cadastro") {
+            // acesso para o room
+            val context = LocalContext.current
+            val banco = ImovelDatabase.getDatabase(context)
+            val dao = banco.imovelDao()
+
+            // viewmodel + DAO, conecção
+            val imovelViewModel: ImovelViewModel = viewModel(
+                factory = object : ViewModelProvider.Factory {
+                    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                        return ImovelViewModel(dao) as T
+                    }
+                }
+            )
+
+            // direcionando viewmodel para tela de cadastro para salvar os dados
+            CadastroScreen(
+                viewModel = imovelViewModel,
+                onNavigateBack = {
+                    navController.popBackStack() // Fecha o cadastro e volta pra Home
+                }
+            )
         }
     }
 }
